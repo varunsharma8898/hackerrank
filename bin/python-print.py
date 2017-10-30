41bde780d81315e815c32703ad55fd8105f2974e
Sharma, Varun <varunsharma8898@gmail.com>
2017-10-30 16:47:13 +0530

    Solving easy challenges - python-print

---
+++ bin/python-print.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+for i in range(1, int(input()) + 1):
+    print(i, end="")
+
+'''
+Read an integer N. Without using any string methods, try to print the following:
+123...N
+
+Sample Input:
+3
+
+Sample Output:
+123
+'''
