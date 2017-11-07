ad52a0e648167e4229fbceae41f47bf667a3aa26
Sharma, Varun <varunsharma8898@gmail.com>
2017-11-07 13:01:01 +0530

    Solving easy challenges - triangle-quest

--- bin/triangle_quest.py
+++ bin/triangle_quest.py
@@ -1,31 +1,22 @@
-for i in range(1,int(input())+1):
-    print(((10 ** i - 1) // 9) ** 2)
-
+for i in range(1,int(input())): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print((10 ** i - 1) * i // 9)
 
 '''
-You are given a positive integer . 
-Your task is to print a palindromic triangle of size .
-
-For example, a palindromic triangle of size  is:
+You are given a positive integer N. Print a numerical triangle of height (N - 1) like the one below:
 
 1
-121
-12321
-1234321
-123454321
-You can't take more than two lines. The first line (a for-statement) is already written for you. 
-You have to complete the code using exactly one print statement.
+22
+333
+4444
+55555
+......
 
-Note: 
-Using anything related to strings will give a score of . 
-Using more than one for-statement will give a score of .
-More than 2 lines will result in 0 score. Do not leave a blank line also
+Sample Input:
+5
 
-Solution:
-1 * 1 = 1
-11 * 11 = 121
-111 * 111 = 12321
-1111 * 1111 = 1234321
-...
-
+Sample Output:
+1
+22
+333
+4444
 '''
