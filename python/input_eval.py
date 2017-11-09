8cf5d0da308a9b0ebcc110469f269132b624d738
Sharma, Varun <varunsharma8898@gmail.com>
2017-11-09 12:49:54 +0530

    builtins - input and eval

---
+++ python/input_eval.py
@@ -0,0 +1,22 @@
+x, k = list(map(int, input().split()))
+if eval(input()) == k:
+    print(True)
+else:
+    print(False)
+
+
+
+'''
+Output Format
+
+Print True if . Otherwise, print False.
+
+Sample Input
+
+1 4
+x**3 + x**2 + x + 1
+Sample Output
+
+True
+
+'''
