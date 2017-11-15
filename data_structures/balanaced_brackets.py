3f722330a7e3cf5b950ddd7cde39486528407191
Sharma, Varun <varunsharma8898@gmail.com>
2017-11-15 18:18:13 +0530

    Solving DS problem - corrected balanced brackets

--- data_structures/balanaced_brackets.py
+++ data_structures/balanaced_brackets.py
@@ -2,30 +2,6 @@
 
 import sys
 
-left_brackets = list("([{")
-right_brackets = {
-    '[': ']',
-    '{': '}',
-    '(': ')'
-}
-
-def isBalanced(s):
-    mystack = []
-    balanced = 1
-    for char in list(s):
-        if balanced == 0:
-            continue
-
-        if char in left_brackets:
-            mystack.append(char)
-        else:
-            if right_brackets[mystack[-1]] == char:
-                mystack.pop()
-                continue
-            else:
-                balanced = 0
-
-    return "YES" if balanced == 1 else "NO"
 
 def isValid(s):
     mystack = []
@@ -39,16 +15,8 @@ def isValid(s):
             mystack.pop()
         else:
             mystack.append(x)
-        # if not mystack:
-        #     mystack.append(x)
-        # elif x in bracket_map:
-        #     mystack.append(x)
-        # else:
-        #     if x == bracket_map[mystack[-1]]:
-        #         mystack.pop()
-        #     else:
-        #         return "NO"
 
+    # If stack is empty, it is balanced.
     return "YES" if not mystack else "NO"
 
 
@@ -56,7 +24,6 @@ if __name__ == "__main__":
     t = int(input().strip())
     for a0 in range(t):
         s = input().strip()
-        # result = isBalanced(s)
         result = isValid(s)
         print(result)
 
